e08729d3f483d58ff0b1a83e8e563b51d87af985
Lucas Loza <lucasloza2275@gmail.com>
2023-02-27 22:14:01 -0300

    Proyecto Argentina Programa

---
+++ CVjs.js
@@ -0,0 +1,12 @@
+
+function moverseA(Formación) {
+    location.hash = "#" + Formación;
+  }
+
+function moverseA(Experiencia) {
+    location.hash = "#" + Experiencia;
+  }
+
+function moverseA(Contacto) {
+    location.hash = "#" + Contacto;
+  }
